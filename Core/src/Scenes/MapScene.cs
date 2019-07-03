3de9465e3df8fab01a8a0e12fec51e227146e963
Xeltica <xeltica@gmail.com>
2019-07-03 18:58:03 +0900

    全マップ見られるように

--- Core/src/Scenes/MapScene.cs
+++ Core/src/Scenes/MapScene.cs
@@ -21,6 +21,9 @@ namespace NeoDefenderEngine
                 area = lvldat.FirstArea;
             var areadat = Load<AreaData>($"./Resources/Levels/Level {level}/Area {1}/area.json");
 
+            l = level;
+            a = area;
+
             Root.Scale = Vector.One * 2;
             Root.Add(Sprite.LoadFrom($"Resources/Graphics/{areadat.BG}"));
 
@@ -68,6 +71,30 @@ namespace NeoDefenderEngine
                 frontMap.Location += delta / 2;
                 backMap.Location += delta / 2;
             }
+
+            if (Input.Keyboard.Space.IsKeyDown)
+            {
+                var d = new Dictionary<string, object>();
+                if (l == 5)
+                {
+                    if (a == 1)
+                    {
+                        d["level"] = 5;
+                        d["area"] = 2;
+                    }
+                    else
+                    {
+                        d["level"] = 1;
+                        d["area"] = 1;
+                    }
+                }
+                else
+                {
+                    d["level"] = l + 1;
+                    d["area"] = 1;
+                }
+                router.ChangeScene<MapScene>(d);
+            }
             
             frontMap.Location += vec * 128 * (float)e.DeltaTime;
             backMap.Location += vec * 128 * (float)e.DeltaTime;
@@ -91,48 +118,8 @@ namespace NeoDefenderEngine
         private Texture2D[] textures;
 
         private Point prevPoint;
+
+        private int l, a;
     }
 
-    public static class MapUtility
-	{
-		public static void Save(byte[,,] array, string path)
-		{
-			var w = array.GetLength(0);
-			var h = array.GetLength(1);
-
-			using(var bw = new BinaryWriter(new FileStream(path, FileMode.Create)))
-            {
-                bw.Write("CITCHIP".AsSpan());
-                bw.Write(w);
-                bw.Write(h);
-
-                for (var z = 0; z < 2; z++)
-                    for (var y = 0; y < h; y++)
-                        for (var x = 0; x < w; x++)
-                            bw.Write(array[x, y, z]);
-            }
-		}
-
-		public static byte[,,] Load(string path)
-		{
-			using (var br = new BinaryReader(new FileStream(path, FileMode.Open)))
-            {
-                if (new string(br.ReadChars(7)) != "CITCHIP")
-                {
-                    br.Close();
-                    throw new Exception("指定したファイルは、有効な Defender Story マップファイルではありません。");
-                }
-                var w = br.ReadInt32();
-                var h = br.ReadInt32();
-
-                var array = new byte[w, h, 2];
-
-                for (var z = 0; z < 2; z++)
-                    for (var y = 0; y < h; y++)
-                        for (var x = 0; x < w; x++)
-                            array[x, y, z] = br.ReadByte();
-                return array;
-            }
-		}
-	}
 }
